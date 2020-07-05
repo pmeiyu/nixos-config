3c3ee0beae4a38c6421b387330e034899e7ae39a
Peng Mei Yu <i@pengmeiyu.com>
2020-07-05 12:13:45 +0800

    pkgs: go-shadowsocks2: Comply with new go build system.
    * pkgs/go-shadowsocks2.nix (go-shadowsocks2): Comply with new go build system.

--- pkgs/go-shadowsocks2.nix
+++ pkgs/go-shadowsocks2.nix
@@ -11,11 +11,11 @@ buildGoModule rec {
     sha256 = "0v7yirl2zkp67c9h22n61vckkawad354nsvyn9irvk5426c7zfid";
   };
 
-  modSha256 = "0avlnbj8s6wnmgsavivyhk16wkh2ns0lp7xc1pi9rg2pkymcdmz7";
+  vendorSha256 = "0avlnbj8s6wnmgsavivyhk16wkh2ns0lp7xc1pi9rg2pkymcdmz7";
 
   meta = with stdenv.lib; {
     description = "Next-generation Shadowsocks in Go";
-    homepage = "https://github.com/shadowsocks/go-shadowsocks";
+    homepage = "https://github.com/shadowsocks/go-shadowsocks2";
     license = licenses.asl20;
     platforms = platforms.all;
   };
