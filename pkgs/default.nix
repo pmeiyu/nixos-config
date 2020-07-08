b1260126217c4ce70db93178db98a9d0de72d7d4
Peng Mei Yu <i@pengmeiyu.com>
2020-07-08 16:15:54 +0800

    pkgs: kcptun: New package.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,6 +3,7 @@ in {
   dnsmasq-china-list = pkgs.callPackage ./dnsmasq-china-list.nix { };
   go-shadowsocks2 = pkgs.callPackage ./go-shadowsocks2.nix { };
   ibus-engines.rime = pkgs.callPackage ./ibus-rime.nix { };
+  kcptun = pkgs.callPackage ./kcptun.nix { };
   steven-black-hosts = pkgs.callPackage ./steven-black-hosts.nix { };
   v2ray-plugin = pkgs.callPackage ./v2ray-plugin.nix { };
   vlmcsd = pkgs.callPackage ./vlmcsd.nix { };
---
+++ pkgs/kcptun.nix
@@ -0,0 +1,27 @@
+{ stdenv, lib, fetchFromGitHub, buildGoModule }:
+
+buildGoModule rec {
+  pname = "kcptun";
+  version = "20200701";
+
+  src = fetchFromGitHub {
+    owner = "xtaci";
+    repo = "kcptun";
+    rev = "v${version}";
+    sha256 = "1icw6ih31h1hwkx7mz717d6y490fpmawvibaiw4vac8xisq32613";
+  };
+
+  vendorSha256 = "1gdikcpb6cgdacmb725nyvxijv2ccm4v0p8chsk57m5p62zb4cm5";
+
+  postInstall = ''
+    mv $out/bin/client $out/bin/kcptun-client
+    mv $out/bin/server $out/bin/kcptun-server
+  '';
+
+  meta = with stdenv.lib; {
+    description = "Stable and secure tunnel with multiplexing and FEC";
+    homepage = "https://github.com/xtaci/kcptun";
+    license = licenses.mit;
+    platforms = platforms.all;
+  };
+}
