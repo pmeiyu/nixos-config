c2f8707232cbb3902195793b8d0f584f4e7816b4
Peng Mei Yu <i@pengmeiyu.com>
2020-07-21 17:02:13 +0800

    pkgs: steven-black-hosts: Build unbound rules.

--- pkgs/steven-black-hosts.nix
+++ pkgs/steven-black-hosts.nix
@@ -11,9 +11,39 @@ stdenv.mkDerivation rec {
     sha256 = "0nhh37khgkmgpqv9126qji2q0f1ha0000sj948jj28al4ig48cxh";
   };
 
+  buildPhase = ''
+    mkdir -p build/unbound
+
+    cp hosts build/ad
+
+    for i in fakenews gambling porn social; do
+        find extensions/$i -name hosts -exec cat '{}' >>build/$i \;
+    done
+
+
+    ## Unbound config
+
+    echo 'view:' >build/unbound/ad.conf
+    echo 'name: "block-ad"' >>build/unbound/ad.conf
+    awk '/^0\.0\.0\.0|^127\.0\.0\.1/ {
+        print "local-data: \"" $2 " 60 IN A 0.0.0.0\"";
+        print "local-data: \"" $2 " 60 IN AAAA ::1\""
+    }' hosts >>build/unbound/ad.conf
+
+    for i in fakenews gambling porn social; do
+        echo 'view:' >build/unbound/$i.conf
+        echo "name: \"block-$i\"" >>build/unbound/$i.conf
+        find extensions/$i -name hosts -exec \
+            awk '/^0\.0\.0\.0|^127\.0\.0\.1/ {
+                print "local-data: \"" $2 " 60 IN A 0.0.0.0\"";
+                print "local-data: \"" $2 " 60 IN AAAA ::1\""
+            }' '{}' >>build/unbound/$i.conf \;
+    done
+  '';
+
   installPhase = ''
     mkdir -p $out
-    cp -rv * $out
+    cp -rv build/* $out
   '';
 
   meta = with stdenv.lib; {
