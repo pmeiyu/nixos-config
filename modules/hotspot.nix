e17fba47c818fcaca386b84de600302bf01a7af7
Peng Mei Yu <i@pengmeiyu.com>
2020-12-09 21:58:00 +0800

    module: hotspot: Add DNS block lists.

--- modules/hotspot.nix
+++ modules/hotspot.nix
@@ -19,6 +19,11 @@ in {
         type = types.str;
         description = "WPA password.";
       };
+      block.ad = mkEnableOption "Block ad.";
+      block.fake-news = mkEnableOption "Block fake news.";
+      block.gambling = mkEnableOption "Block gambling.";
+      block.porn = mkEnableOption "Block porn.";
+      block.social = mkEnableOption "Block social networks.";
     };
   };
 
@@ -92,6 +97,34 @@ in {
     services.unbound = {
       interfaces = [ "10.10.0.1" ];
       allowedAccess = [ "10.10.0.0/16" "fd00:10::/64" ];
+      extraConfig = ''
+        server:
+
+        ${optionalString cfg.block.ad ''
+          access-control-view: 10.10.0.0/16 block-ad
+          access-control-view: fd00:10::/64 block-ad
+        ''}
+
+        ${optionalString cfg.block.fake-news ''
+          access-control-view: 10.10.0.0/16 block-fakenews
+          access-control-view: fd00:10::/64 block-fakenews
+        ''}
+
+        ${optionalString cfg.block.gambling ''
+          access-control-view: 10.10.0.0/16 block-gambling
+          access-control-view: fd00:10::/64 block-gambling
+        ''}
+
+        ${optionalString cfg.block.porn ''
+          access-control-view: 10.10.0.0/16 block-porn
+          access-control-view: fd00:10::/64 block-porn
+        ''}
+
+        ${optionalString cfg.block.social ''
+          access-control-view: 10.10.0.0/16 block-social
+          access-control-view: fd00:10::/64 block-social
+        ''}
+      '';
     };
 
     networking.firewall = {
@@ -100,11 +133,18 @@ in {
         allowedUDPPorts = [ 53 ];
       };
       extraCommands = ''
+        # Hijack DNS requests.
+        iptables -w -t nat -A PREROUTING -s 10.10.0.0/24 ! -d 10.10.0.1 -p udp --dport 53 -j DNAT --to 127.0.0.1:53
+        iptables -w -t nat -A PREROUTING -s 10.10.0.0/24 ! -d 10.10.0.1 -p tcp --dport 53 -j DNAT --to 127.0.0.1:53
+
         iptables -w -t nat -A POSTROUTING -s 10.10.0.0/24 -j MASQUERADE
         iptables -w -A FORWARD -i ${cfg.interface} -s 10.10.0.0/24 -j ACCEPT
         iptables -w -A FORWARD -o ${cfg.interface} -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
       '';
       extraStopCommands = ''
+        iptables -w -t nat -D PREROUTING -s 10.10.0.0/24 ! -d 10.10.0.1 -p udp --dport 53 -j DNAT --to 127.0.0.1:53
+        iptables -w -t nat -D PREROUTING -s 10.10.0.0/24 ! -d 10.10.0.1 -p tcp --dport 53 -j DNAT --to 127.0.0.1:53
+
         iptables -w -t nat -D POSTROUTING -s 10.10.0.0/24 -j MASQUERADE
         iptables -w -D FORWARD -i ${cfg.interface} -s 10.10.0.0/24 -j ACCEPT
         iptables -w -D FORWARD -o ${cfg.interface} -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
