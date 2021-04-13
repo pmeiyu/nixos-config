db33af5eb2ad0080ea801278f7714e3a67a63270
Peng Mei Yu <pengmyu@gmail.com>
2021-04-13 15:34:32 +0800

    snippet: thinkpad-x270: Add sensors config.

--- snippets/thinkpad-x270.nix
+++ snippets/thinkpad-x270.nix
@@ -42,4 +42,25 @@
       STOP_CHARGE_THRESH_BAT1 = 80;
     };
   };
+
+  environment.etc."sensors.d/thinkpad.conf" = {
+    text = ''
+      chip "thinkpad-isa-0000"
+          ignore temp2
+          ignore temp3
+          ignore temp4
+          ignore temp5
+          ignore temp6
+          ignore temp7
+          ignore temp8
+          ignore temp9
+          ignore temp10
+          ignore temp11
+          ignore temp12
+          ignore temp13
+          ignore temp14
+          ignore temp15
+          ignore temp16
+    '';
+  };
 }
