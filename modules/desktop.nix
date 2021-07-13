fe75292a125067b75c86d6f970cf698dc8a2e698
Peng Mei Yu <pmy@xqzp.net>
2021-07-13 17:30:05 +0800

    module: desktop: Add USTC binary cache mirror.

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -25,6 +25,7 @@ in
     system.stateVersion = mkDefault "9999.99";
 
     nix.binaryCaches = [
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://mirrors.bfsu.edu.cn/nix-channels/store/"
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store/"
