ef0864aadcc3fb872e733f350fe16290c8b733cc
Peng Mei Yu <pmy@xqzp.net>
2021-11-29 18:30:20 +0800

    module: weechat: Add weechat-matrix script.

--- modules/weechat.nix
+++ modules/weechat.nix
@@ -16,14 +16,34 @@ in
   };
 
   config = mkIf cfg.enable {
+    nixpkgs.config.packageOverrides = pkgs: rec {
+      weechat = pkgs.weechat.override {
+        configure = { availablePlugins, ... }: {
+          scripts = with pkgs.weechatScripts; [
+            weechat-matrix
+          ];
+        };
+      };
+    };
+
+    environment.systemPackages = [
+      (pkgs.writeScriptBin "chat" ''
+        #!${pkgs.runtimeShell}
+        export TERM=xterm-256color
+        tmux -S /tmp/tmux-${
+          toString config.users.users.meiyu.uid
+        }/default attach -t weechat
+      '')
+    ];
+
     systemd.services.weechat = {
       serviceConfig = {
         Type = "oneshot";
         User = "meiyu";
         RemainAfterExit = "yes";
         ExecStart =
-          "${pkgs.tmux}/bin/tmux -2 new-session -d -s irc ${pkgs.weechat}/bin/weechat";
-        ExecStop = "${pkgs.tmux}/bin/tmux kill-session -t irc";
+          "${pkgs.tmux}/bin/tmux -2 new-session -d -s weechat ${pkgs.weechat}/bin/weechat";
+        ExecStop = "${pkgs.tmux}/bin/tmux kill-session -t weechat";
       };
       wants = [ "network.target" ];
       wantedBy = [ "multi-user.target" ];
@@ -38,15 +58,5 @@ in
         '';
       };
     };
-
-    environment.systemPackages = [
-      (pkgs.writeScriptBin "irc" ''
-        #!${pkgs.runtimeShell}
-        export TERM=xterm-256color
-        tmux -S /tmp/tmux-${
-          toString config.users.users.meiyu.uid
-        }/default attach -t irc
-      '')
-    ];
   };
 }
