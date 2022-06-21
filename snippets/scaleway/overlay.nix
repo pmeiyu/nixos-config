94b2d865abedf769a4e0c9b4539dbb1d3ee22e3c
Peng Mei Yu <pmy@xqzp.net>
2022-06-21 16:41:10 +0800

    snippet: Add scaleway.

---
+++ snippets/scaleway/overlay.nix
@@ -0,0 +1,32 @@
+self: super: with super; {
+  scaleway = stdenv.mkDerivation {
+    pname = "scaleway";
+    version = "2019.04.30";
+
+    src = fetchFromGitHub {
+      owner = "scaleway";
+      repo = "image-tools";
+      rev = "8d224da40fca3df75215109a98e17cbc4e16c7ae";
+      hash = "sha256-Cu1zmoHIhjlz+59pg2DteM9pxFW/yP4Rs6pNAiUXB50=";
+    };
+
+    buildInputs = [ curl makeWrapper ];
+
+    buildPhase = "true";
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp -rv bases/overlay-common/usr/local/bin/scw-metadata $out/bin
+      cp -rv bases/overlay-common/usr/local/bin/scw-metadata-json $out/bin
+
+      runHook postInstall
+    '';
+
+    postInstall = ''
+      wrapProgram $out/bin/scw-metadata \
+          --prefix PATH : ${lib.makeBinPath [ curl ]}
+      wrapProgram $out/bin/scw-metadata-json \
+          --prefix PATH : ${lib.makeBinPath [ curl ]}
+    '';
+  };
+}
