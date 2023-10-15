9f0fb0d77155c200afa2149431c0ea31566c5eea
Peng Mei Yu <pmy@xqzp.net>
2023-10-15 11:37:25 +0800

    snippet: gpu: nvidia: Enable prime offloading.

--- snippets/gpu/nvidia.nix
+++ snippets/gpu/nvidia.nix
@@ -3,7 +3,18 @@
 {
   services.xserver.videoDrivers = [ "nvidia" ];
 
-  hardware.nvidia.open = true;
+  hardware.nvidia = {
+    modesetting.enable = true;
+
+    open = false;
+
+    prime = {
+      offload.enable = true;
+      offload.enableOffloadCmd = true;
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
 
   programs.sway.extraOptions = [ "--unsupported-gpu" ];
 }
